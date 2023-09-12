ad1b025994335aa44415d41c9a7bdc70db55e92f
Akash Kulshrestha <akashkulshrestha7@gmail.com>
2023-09-13 01:22:20 +0530

    1. Get Date After x Days

---
+++ Introduction to NODE JS/Practice 2/1. Get Date After x Days/index.js
@@ -0,0 +1,14 @@
+//node install date-fns --save
+
+const addDays = require("date-fns/addDays");
+
+const getDateAfterXDays = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+};
+
+module.exports = getDateAfterXDays;
+
+console.log(getDateAfterXDays(5));
